632109a8a4815322d31af06fc2bb41ce0356b015
Muhammed Thanish <mnmtanish@gmail.com>
2014-06-02 17:41:48 +0530

    Add browser prefixes and tweak pageHeight plugin to work with firefox
    It seems Firefox only parses inline style tags at page load. Style tags
    added later does not get parsed. (Must check specs about this behavior).
    Added a function to set min-height by JavaScript if browser isn't
    Google Chrome.

--- scripts/ph.js
+++ scripts/ph.js
@@ -2,6 +2,9 @@
 
     'use strict';
 
+    // http://stackoverflow.com/a/4565120/527922
+    var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+
     window.pH = {};
     var container = null;
     var classes = {};
@@ -17,13 +20,23 @@
 
     function updateTag() {
         var results = [], idx = 0;
-        for (var name in classes)
-            results[idx++] = name+'{min-height:'+classes[name](window.innerHeight)+'px;}';
+        for (var name in classes) {
+            var height = classes[name](window.innerHeight);
+            results[idx++] = name+'{min-height:'+height+'px;}';
+            is_chrome || updateElements(name, height);
+        }
         container.innerText = results.join('');
     }
 
     function addClass(name, fn) {
         classes[name] = typeof fn === 'function' ? fn : function (h) { return h };
+    }
+
+    function updateElements (name, height) {
+        var elements = document.querySelectorAll(name);
+        for(var i=elements.length; i-->0;) {
+            elements[i].style.minHeight = height+'px';
+        }
     }
 
 })();
